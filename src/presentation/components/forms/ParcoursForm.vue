3e0c397441162f8c2b72529036e13b18fc4cd9e0
TIBA-Sarah-Kaouthar <kaouthar.sarah.tiba@etud.u-picardie.fr>
2025-12-01 16:36:19 +0100

    Ajout formulaire pour ajouter un parcours, toujours erreur pour enregistrement de parcours. "erreur Impossible de creer le nouveau parcours localhost:5137."

--- src/presentation/components/forms/ParcoursForm.vue
+++ src/presentation/components/forms/ParcoursForm.vue
@@ -4,6 +4,7 @@ import { BootstrapButtonEnum } from '@/types/BootstrapButtonEnum';
 import { Parcours } from '@/domain/entities/Parcours';
 import CustomInput from '@/presentation/components/forms/components/CustomInput.vue';
 import CustomButton from '@/presentation/components/forms/components/CustomButton.vue';
+import { ParcoursDAO } from '@/domain/daos/ParcoursDAO';
 
 const currentParcours = ref<Parcours>(new Parcours(null, null, null));
 const isOpen = ref(false);
@@ -28,6 +29,22 @@ const props = defineProps({
     default: null,
   },
 });
+const saveParcours = () => {
+  if (currentParcours.value.ID) {
+    // Mise à jour d'un parcours (tu feras plus tard)
+  } else {
+    ParcoursDAO.getInstance()
+        .create(currentParcours.value)
+        .then(() => {
+          alert('Parcours créé avec succès');
+          closeForm();
+        })
+        .catch((ex) => {
+          alert(ex.message);
+        });
+  }
+};
+
 
 onBeforeMount(() => {
   if (props.parcours) {
@@ -40,3 +57,58 @@ defineExpose({
   closeForm,
 });
 </script>
+<template>
+  <div v-if="isOpen" class="custom-modal">
+    <div class="card new-parcours">
+      <div class="card-header" style="background: #273656">
+        <template v-if="parcours && parcours.ID"> Modification du parcours </template>
+        <template v-else> Nouveau parcours </template>
+      </div>
+      <div class="card-body">
+        <div class="card-text mt-1 mb-1">
+          <form>
+            <CustomInput id="intitule" libelle="Intitulé" type="text" placeholder="Intitulé du parcours" />
+            <CustomInput class="mt-2" id="annee" libelle="Année" type="number"
+                         placeholder="Année de formation" />
+          </form>
+        </div>
+        <CustomButton class="mt-1" style="margin-left: 5px" :color="BootstrapButtonEnum.danger"
+                      @click="closeForm">
+          Annuler
+        </CustomButton>
+        <CustomButton class="mt-1" style="margin-left: 5px" :color="BootstrapButtonEnum.primary" @click="saveParcours">
+          Enregistrer
+        </CustomButton>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.custom-modal {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(87, 86, 86, 0.5);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.custom-modal .card {
+  width: 250px;
+}
+
+.card-header {
+  background: #273656;
+  color: white;
+  text-align: left;
+}
+
+.card-text {
+  text-align: left;
+}
+</style> 
