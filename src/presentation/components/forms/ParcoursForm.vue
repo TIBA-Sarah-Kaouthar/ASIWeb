959c0fd09afb3e1ed081ed6811637a2109c72c7b
TIBA-Sarah-Kaouthar <kaouthar.sarah.tiba@etud.u-picardie.fr>
2025-12-01 15:58:57 +0100

    2eme action, creation de custom buttonpour creation de champs dans le formulaire. Création du formulaire avec Parcours.ts, et ParcoursForm.vue creation form

---
+++ src/presentation/components/forms/ParcoursForm.vue
@@ -0,0 +1,42 @@
+﻿<script setup lang="ts">
+import { ref, onBeforeMount, defineExpose, defineProps, toRaw } from 'vue';
+import { BootstrapButtonEnum } from '@/types/BootstrapButtonEnum';
+import { Parcours } from '@/domain/entities/Parcours';
+import CustomInput from '@/presentation/components/forms/components/CustomInput.vue';
+import CustomButton from '@/presentation/components/forms/components/CustomButton.vue';
+
+const currentParcours = ref<Parcours>(new Parcours(null, null, null));
+const isOpen = ref(false);
+
+const openForm = (parcours: Parcours | null = null) => {
+  isOpen.value = true;
+
+  if (parcours) {
+    currentParcours.value = structuredClone(toRaw(parcours));
+  }
+};
+
+const closeForm = () => {
+  isOpen.value = false;
+  currentParcours.value = new Parcours(null, null, null);
+};
+
+const props = defineProps({
+  parcours: {
+    type: Object as () => Parcours | null,
+    required: false,
+    default: null,
+  },
+});
+
+onBeforeMount(() => {
+  if (props.parcours) {
+    currentParcours.value = props.parcours;
+  }
+});
+
+defineExpose({
+  openForm,
+  closeForm,
+});
+</script>
---
+++ src/presentation/components/forms/components/CustomInput.vue
@@ -0,0 +1,17 @@
+﻿<script setup lang="ts">
+const model = defineModel();
+
+const props = defineProps<{
+  id: string;
+  libelle: string;
+  type: string;
+  placeholder: string | undefined;
+}>();
+</script>
+
+<template>
+  <div class="form-group">
+    <label for="intitule">{{ libelle }} :</label>
+    <input v-model="model" class="form-control" :type="type" :id="id" :placeholder="placeholder" />
+  </div>
+</template>
